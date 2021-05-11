082c9ce733eda471f5d8db7c800b708d4b558f71
sergiuradu5 <sergiuradu5@gmail.com>
2021-05-11 17:43:56 +0200

    Added Swipe Left & Right Animations with HammerJS and Angular Animations

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -112,7 +112,8 @@ export function tokenGetter() {
                allowedDomains: ['localhost:5000'],
                disallowedRoutes: ['localhost:5000/api/auth']
             }
-      })
+      }),
+      HammerModule
    ],
    providers: [
       AuthService,
--- DatingApp-SPA/src/app/members/member-card-detailed/member-card-detailed.component.ts
+++ DatingApp-SPA/src/app/members/member-card-detailed/member-card-detailed.component.ts
@@ -3,13 +3,26 @@ import { User } from 'src/app/_models/user';
 import { AuthService } from 'src/app/_services/auth.service';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { trigger, keyframes, animate, transition } from '@angular/animations';
+import * as kf from './keyframes';
 
 @Component({
   selector: 'app-member-card-detailed',
   templateUrl: './member-card-detailed.component.html',
-  styleUrls: ['./member-card-detailed.component.css']
+  styleUrls: ['./member-card-detailed.component.css'],
+  animations: [ 
+    trigger('cardAnimator', [
+      transition('* => wobble', animate(350, keyframes(kf.wobble))),
+      transition('* => flipOutY', animate(350, keyframes(kf.flipOutY))),
+      transition('* => slideOutLeft', animate(350, keyframes(kf.slideOutLeft))),
+      transition('* => slideOutRight', animate(350, keyframes(kf.slideOutRight))),
+      transition('* => swing', animate(350, keyframes(kf.swing))),
+    ])
+  ]
 })
 export class MemberCardDetailedComponent implements OnInit {
+  animationState: string;
+  
   @Input() user: User;
   @Output() skipCurrentUser = new EventEmitter();
 
@@ -18,10 +31,30 @@ export class MemberCardDetailedComponent implements OnInit {
     private userService: UserService,
     private alertify: AlertifyService
   ) { }
+
+  startAnimation(state) {
+    console.log(state)
+    if(!this.animationState) {
+      this.animationState = state
+    }
+  }
+
+  resetAnimationState() {
+    this.animationState = '';
+  }
+
   ngOnInit() {
   }
 
   skipUser() {
+    this.startAnimation('slideOutLeft');
+    setTimeout( () => {
+      this.proceedToNextUser();
+    }, 350);
+    
+  }
+
+  proceedToNextUser() {
     this.skipCurrentUser.emit(this.user.id);
     this.visitUser(this.user.id);
   }
@@ -43,7 +76,11 @@ export class MemberCardDetailedComponent implements OnInit {
       this.alertify.error(error);
     }); 
 
-    this.skipUser();
+    this.startAnimation('slideOutRight');
+    setTimeout(() => {
+      this.proceedToNextUser();
+    }, 350);
+    
   }
 
 }
